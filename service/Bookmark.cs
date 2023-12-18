edf39cf885580402a18ee85a43aae2d6ecdd978a
MarioPerac <peracmario@outlook.com>
2023-12-18 20:03:41 +0100

    Add service classes

---
+++ service/Bookmark.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surfario
+{
+    public class Bookmark
+    {
+        public Bookmark(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Bookmark bookmark &&
+                   Address == bookmark.Address;
+        }
+    }
+}
---
+++ service/BookmarkManager.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Windows;
+
+namespace Surfario.service
+{
+    public class BookmarkManager
+    {
+        public static void SaveBookmarksToFile(List<Bookmark> bookmarks)
+        {
+            string json = JsonConvert.SerializeObject(bookmarks);
+
+
+            File.WriteAllText("bookmarks.json", json);
+        }
+
+        public static List<Bookmark> LoadBookmarksFromFile()
+        {
+            List<Bookmark> bookmarks = new List<Bookmark>();
+            try
+            {
+                string json = File.ReadAllText("bookmarks.json");
+
+                bookmarks = JsonConvert.DeserializeObject<List<Bookmark>>(json);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return bookmarks;
+        }
+    }
+}
+
